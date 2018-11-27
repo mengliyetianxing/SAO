8b86f5af2227fa163b4397d2e1a1d8726a10e314
mengliyetianxing <2471794405@qq.com>
2018-11-27 14:45:30 +0800

    邮箱验证码功能实现

---
+++ src/main/java/com/util/Email.java
@@ -0,0 +1,51 @@
+package com.util;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Email {
+	/**
+	 * 发送邮件
+     * @param to 给谁发
+     * @param text 发送内容
+     */
+    public static void sendEmail(String to,String text) throws MessagingException {
+        //创建连接对象 连接到邮件服务器
+        Properties properties = new Properties();
+        //设置发送邮件的基本参数
+        //发送邮件服务器
+        properties.put("mail.smtp.host", "smtp.163.com");
+        //发送端口
+        properties.put("mail.smtp.port", "25");
+        properties.put("mail.smtp.auth", "true");
+        //设置发送邮件的账号和密码
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                //两个参数分别是发送邮件的账户和密码
+                return new PasswordAuthentication("liantianyi2015@163.com","wsl666");
+            }
+        });
+        //创建邮件对象
+        Message message = new MimeMessage(session);
+        //设置发件人
+        message.setFrom(new InternetAddress("liantianyi2015@163.com"));
+        //设置收件人
+        message.setRecipient(Message.RecipientType.TO,new InternetAddress(to));
+        //设置主题
+        message.setSubject("验证码");
+        //设置邮件正文  第二个参数是邮件发送的类型
+        message.setContent(text,"text/html;charset=UTF-8");
+        //发送一封邮件
+        Transport.send(message);
+    }
+
+}
--- src/test/java/com/SAO/Test.java
+++ src/test/java/com/SAO/Test.java
@@ -1,11 +1,21 @@
 package com.SAO;
 
+import javax.mail.MessagingException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.util.Email;
 import com.util.MD5;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(MD5.md5("root"));
+		try {
+				Email.sendEmail("597137930@qq.com", "你这个沙雕");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
